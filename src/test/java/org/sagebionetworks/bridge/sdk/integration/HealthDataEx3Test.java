e4611aca7e16ea544b864c7d62209e55d469e396
Dwayne Jeng <dwayne.jeng@sagebase.org>
2021-08-20 17:00:48 -0700

    Integ Tests for Exporter 3.0 initial implementation

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/HealthDataEx3Test.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/HealthDataEx3Test.java
@@ -13,38 +13,53 @@ import java.util.stream.Collectors;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import org.sagebionetworks.bridge.rest.api.ForSuperadminsApi;
+import org.sagebionetworks.bridge.rest.api.ForWorkersApi;
 import org.sagebionetworks.bridge.rest.api.ParticipantsApi;
+import org.sagebionetworks.bridge.rest.api.StudiesApi;
 import org.sagebionetworks.bridge.rest.exceptions.EntityNotFoundException;
 import org.sagebionetworks.bridge.rest.model.HealthDataRecordEx3;
 import org.sagebionetworks.bridge.rest.model.HealthDataRecordEx3List;
+import org.sagebionetworks.bridge.rest.model.Role;
+import org.sagebionetworks.bridge.rest.model.Study;
 import org.sagebionetworks.bridge.user.TestUserHelper;
 import org.sagebionetworks.bridge.util.IntegTestUtils;
 
 @SuppressWarnings("ConstantConditions")
 public class HealthDataEx3Test {
-    private static final String DUMMY_STUDY_ID = "dummy-study";
     private static final String TEST_CLIENT_INFO = "Integration Tests";
 
     private static TestUserHelper.TestUser admin;
     private static DateTime createdOn;
+    private static Study study;
+    private static String studyId;
+    private static TestUserHelper.TestUser worker;
 
     private TestUserHelper.TestUser user;
     private String userHealthCode;
 
     @BeforeClass
-    public static void beforeClass() {
+    public static void beforeClass() throws Exception {
         admin = TestUserHelper.getSignedInAdmin();
+        worker = TestUserHelper.createAndSignInUser(HealthDataEx3Test.class, false, Role.WORKER);
         createdOn = DateTime.now(DateTimeZone.UTC);
+
+        // Create study.
+        StudiesApi studiesApi = admin.getClient(StudiesApi.class);
+        studyId = Tests.randomIdentifier(HealthDataEx3Test.class);
+        study = new Study().identifier(studyId).name(studyId);
+        studiesApi.createStudy(study).execute();
+        study = studiesApi.getStudy(studyId).execute().body();
     }
 
     @Before
     public void before() throws Exception {
-        user = TestUserHelper.createAndSignInUser(SurveyTest.class, true);
+        user = TestUserHelper.createAndSignInUser(HealthDataEx3Test.class, true);
         userHealthCode = admin.getClient(ParticipantsApi.class).getParticipantById(user.getUserId(), false)
                 .execute().body().getHealthCode();
     }
@@ -56,9 +71,21 @@ public class HealthDataEx3Test {
         }
     }
 
+    @AfterClass
+    public static void afterClass() throws Exception {
+        if (study != null) {
+            admin.getClient(StudiesApi.class).deleteStudy(study.getIdentifier(), true).execute();
+        }
+
+        if (worker != null) {
+            worker.signOutAndDeleteUser();
+        }
+    }
+
     @Test
     public void test() throws Exception {
         ForSuperadminsApi superadminsApi = admin.getClient(ForSuperadminsApi.class);
+        ForWorkersApi workersApi = worker.getClient(ForWorkersApi.class);
 
         // Create. App ID is set automatically by Bridge. Set the remaining values for test.
         HealthDataRecordEx3 record = new HealthDataRecordEx3();
@@ -67,8 +94,8 @@ public class HealthDataEx3Test {
         record.setExported(false);
         record.setHealthCode(userHealthCode);
         record.putMetadataItem("foo", "foo-value");
-        record.setStudyId(DUMMY_STUDY_ID);
-        record = superadminsApi.createOrUpdateRecordEx3(record).execute().body();
+        record.setStudyId(studyId);
+        record = workersApi.createOrUpdateRecordEx3(IntegTestUtils.TEST_APP_ID, record).execute().body();
         String recordId = record.getId();
         assertNotNull(recordId);
 
@@ -77,19 +104,19 @@ public class HealthDataEx3Test {
         assertEquals(createdOn, record.getCreatedOn());
         assertFalse(record.isExported());
         assertEquals(userHealthCode, record.getHealthCode());
-        assertEquals(DUMMY_STUDY_ID, record.getStudyId());
+        assertEquals(studyId, record.getStudyId());
         assertEquals(1, record.getVersion().intValue());
 
         assertEquals(1, record.getMetadata().size());
         assertEquals("foo-value", record.getMetadata().get("foo"));
 
         // Test get api.
-        HealthDataRecordEx3 retrievedRecord = superadminsApi.getRecordEx3(recordId).execute().body();
+        HealthDataRecordEx3 retrievedRecord = workersApi.getRecordEx3(IntegTestUtils.TEST_APP_ID, recordId).execute().body();
         assertEquals(record, retrievedRecord);
 
         // Make a simple update to test update API.
         record.putMetadataItem("bar", "bar-value");
-        record = superadminsApi.createOrUpdateRecordEx3(record).execute().body();
+        record = workersApi.createOrUpdateRecordEx3(IntegTestUtils.TEST_APP_ID, record).execute().body();
         assertEquals(recordId, record.getId());
         assertEquals(2, record.getVersion().intValue());
 
@@ -103,42 +130,42 @@ public class HealthDataEx3Test {
 
         // List by user.
         HealthDataRecordEx3 expectedRecord = record;
-        Tests.retryHelper(() -> superadminsApi.getRecordsEx3ForUser(user.getUserId(), createdOnStart, createdOnEnd,
+        Tests.retryHelper(() -> workersApi.getRecordsEx3ForUser(IntegTestUtils.TEST_APP_ID, user.getUserId(), createdOnStart, createdOnEnd,
                 null, null).execute().body().getItems(),
                 recordList -> recordList.size() == 1 && expectedRecord.equals(recordList.get(0)));
 
         // List by app. There may be more than one. Filter for the one that we know about.
-        Tests.retryHelper(() -> superadminsApi.getRecordsEx3ForCurrentApp(createdOnStart, createdOnEnd, null,
+        Tests.retryHelper(() -> workersApi.getRecordsEx3ForApp(IntegTestUtils.TEST_APP_ID, createdOnStart, createdOnEnd, null,
                 null).execute().body().getItems().stream().filter(r -> r.getId().equals(recordId))
                 .collect(Collectors.toList()),
                 recordList -> recordList.size() == 1 && expectedRecord.equals(recordList.get(0)));
 
         // List by study. There may be more than one. Filter for the one that we know about.
-        Tests.retryHelper(() -> superadminsApi.getRecordsEx3ForStudy(DUMMY_STUDY_ID, createdOnStart, createdOnEnd,
+        Tests.retryHelper(() -> workersApi.getRecordsEx3ForStudy(IntegTestUtils.TEST_APP_ID, studyId, createdOnStart, createdOnEnd,
                 null, null).execute().body().getItems().stream()
                 .filter(r -> r.getId().equals(recordId)).collect(Collectors.toList()),
                 recordList -> recordList.size() == 1 && expectedRecord.equals(recordList.get(0)));
 
         // Delete record.
-        superadminsApi.deleteRecordsEx3ForUser(user.getUserId()).execute();
+        superadminsApi.deleteRecordsEx3ForUser(IntegTestUtils.TEST_APP_ID, user.getUserId()).execute();
 
         // Get will now throw.
         try {
-            superadminsApi.getRecordEx3(recordId).execute();
+            workersApi.getRecordEx3(IntegTestUtils.TEST_APP_ID, recordId).execute();
             fail("expected exception");
         } catch (EntityNotFoundException ex) {
             // expected exception
         }
 
         // List by user will now return an empty list.
-        Tests.retryHelper(() -> superadminsApi.getRecordsEx3ForUser(user.getUserId(), createdOnStart, createdOnEnd,
+        Tests.retryHelper(() -> workersApi.getRecordsEx3ForUser(IntegTestUtils.TEST_APP_ID, user.getUserId(), createdOnStart, createdOnEnd,
                 null, null).execute().body().getItems(),
                 List::isEmpty);
     }
 
     @Test
     public void testPagination() throws Exception {
-        ForSuperadminsApi superadminsApi = admin.getClient(ForSuperadminsApi.class);
+        ForWorkersApi workersApi = worker.getClient(ForWorkersApi.class);
 
         // Create 5 health datas for the user. healthCode and createdOn are the only values required.
         // App ID is automatically set by the server. Add study ID for tests.
@@ -150,24 +177,24 @@ public class HealthDataEx3Test {
             recordArray[i] = new HealthDataRecordEx3();
             recordArray[i].setCreatedOn(DateTime.now());
             recordArray[i].setHealthCode(userHealthCode);
-            recordArray[i].setStudyId(DUMMY_STUDY_ID);
-            recordArray[i] = superadminsApi.createOrUpdateRecordEx3(recordArray[i]).execute().body();
+            recordArray[i].setStudyId(studyId);
+            recordArray[i] = workersApi.createOrUpdateRecordEx3(IntegTestUtils.TEST_APP_ID, recordArray[i]).execute().body();
         }
         DateTime createdOnStart = recordArray[0].getCreatedOn();
         DateTime createdOnEnd = recordArray[4].getCreatedOn();
 
         Tests.retryHelper(
-                () -> paginationHelper(nextOffsetKey -> superadminsApi.getRecordsEx3ForUser(user.getUserId(),
+                () -> paginationHelper(nextOffsetKey -> workersApi.getRecordsEx3ForUser(IntegTestUtils.TEST_APP_ID, user.getUserId(),
                         createdOnStart, createdOnEnd, 2, nextOffsetKey).execute().body()),
                 recordList -> paginationValidation(recordArray, recordList));
 
         Tests.retryHelper(
-                () -> paginationHelper(nextOffsetKey -> superadminsApi.getRecordsEx3ForCurrentApp(createdOnStart,
+                () -> paginationHelper(nextOffsetKey -> workersApi.getRecordsEx3ForApp(IntegTestUtils.TEST_APP_ID, createdOnStart,
                         createdOnEnd, 2, nextOffsetKey).execute().body()),
                 recordList -> paginationValidation(recordArray, recordList));
 
         Tests.retryHelper(
-                () -> paginationHelper(nextOffsetKey -> superadminsApi.getRecordsEx3ForStudy(DUMMY_STUDY_ID,
+                () -> paginationHelper(nextOffsetKey -> workersApi.getRecordsEx3ForStudy(IntegTestUtils.TEST_APP_ID, studyId,
                         createdOnStart, createdOnEnd, 2, nextOffsetKey).execute().body()),
                 recordList -> paginationValidation(recordArray, recordList));
     }
