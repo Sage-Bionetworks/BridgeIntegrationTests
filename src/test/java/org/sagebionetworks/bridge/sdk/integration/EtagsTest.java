6e9abcf2e54edf2ae325f241a0999ea8cb6b3680
Alx Dark <alx.dark@sagebase.org>
2022-02-23 11:18:07 -0800

    Updates after code review and some clearer comments

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/EtagsTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/EtagsTest.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.bridge.sdk.integration;
 
+import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.sagebionetworks.bridge.rest.model.Role.STUDY_DESIGNER;
@@ -26,6 +27,11 @@ import retrofit2.Response;
 
 public class EtagsTest {
 
+    private static final String PARTICIPANT_SELF_TIMELINE_URL = "/v5/studies/"+STUDY_ID_1+"/participants/self/timeline";
+    private static final String PARTICIPANT_TIMELINE_URL = "/v5/studies/"+STUDY_ID_1+"/participants/%s/timeline";
+    private static final String TIMELINE_URL = "/v5/studies/"+STUDY_ID_1+"/timeline";
+    private static final String SCHEDULE_URL = "/v5/studies/"+STUDY_ID_1+"/schedule";
+    
     TestUser studyDesigner;
     TestUser user;
     Schedule2 schedule;
@@ -73,51 +79,47 @@ public class EtagsTest {
         
         ForStudyDesignersApi designApi = studyDesigner.getClient(ForStudyDesignersApi.class);
         
+        // First request is a 200, but it has an etag
         Response<Schedule2> res1 = designApi.getScheduleForStudy(STUDY_ID_1).execute();
         assertEquals(200, res1.code());
-        String etag = res1.headers().get("ETag");
+        String etag = res1.headers().get(HttpHeaders.ETAG);
         assertNotNull(etag);
         
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/schedule", etag, 304);
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/timeline", etag, 304);
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/participants/"+user.getUserId()+"/timeline", etag, 304);
-        assertUserStatus("/v5/studies/"+STUDY_ID_1+"/participants/self/timeline", etag, 304);
+        // Request it again through any API, you get a 304.
+        assertStatus(studyDesigner, SCHEDULE_URL, etag, 304);
+        assertStatus(studyDesigner, TIMELINE_URL, etag, 304);
+        assertStatus(studyDesigner, format(PARTICIPANT_TIMELINE_URL, user.getUserId()), etag, 304);
+        assertStatus(user, PARTICIPANT_SELF_TIMELINE_URL, etag, 304);
         
         // Change the schedule, the cache is busted
         schedule.setDuration("P11D");
         schedule = schedulesApi.saveScheduleForStudy(STUDY_ID_1, schedule).execute().body();
         
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/schedule", etag, 200);
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/timeline", etag, 200);
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/participants/"+user.getUserId()+"/timeline", etag, 200);
-        assertUserStatus("/v5/studies/"+STUDY_ID_1+"/participants/self/timeline", etag, 200);
+        // Now you get the content and a 200
+        assertStatus(studyDesigner, SCHEDULE_URL, etag, 200);
+        assertStatus(studyDesigner, TIMELINE_URL, etag, 200);
+        assertStatus(studyDesigner, format(PARTICIPANT_TIMELINE_URL, user.getUserId()), etag, 200);
+        assertStatus(user, PARTICIPANT_SELF_TIMELINE_URL, etag, 200);
         
+        // Get this new etag
         Response<Schedule2> res2 = designApi.getScheduleForStudy(STUDY_ID_1).execute();
-        etag = res2.headers().get("ETag");
+        etag = res2.headers().get(HttpHeaders.ETAG);
         
-        // Deleting the schedule clears the cache too (which was just set) so we see 404 despite sending the
-        // etag.
+        // Deleting the schedule clears the cache too (which was just set) so we see 404 despite 
+        // sending the last etag
         TestUser admin = TestUserHelper.getSignedInAdmin();
         admin.getClient(SchedulesV2Api.class).deleteSchedule(schedule.getGuid()).execute();
         schedule = null;
 
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/schedule", etag, 404);
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/timeline", etag, 404);
-        assertStatus("/v5/studies/"+STUDY_ID_1+"/participants/"+user.getUserId()+"/timeline", etag, 404);
-        assertUserStatus("/v5/studies/"+STUDY_ID_1+"/participants/self/timeline", etag, 404);
+        assertStatus(studyDesigner, SCHEDULE_URL, etag, 404);
+        assertStatus(studyDesigner, TIMELINE_URL, etag, 404);
+        assertStatus(studyDesigner, format(PARTICIPANT_TIMELINE_URL, user.getUserId()), etag, 404);
+        assertStatus(user, PARTICIPANT_SELF_TIMELINE_URL, etag, 404);
     }
     
-    private void assertStatus(String url, String etag, int statusCode) throws IOException { 
+    private void assertStatus(TestUser caller, String url, String etag, int statusCode) throws IOException { 
         HttpResponse response = Request.Get(hostUrl + url)
-            .setHeader("Bridge-Session", studyDesigner.getSession().getSessionToken())
-            .setHeader(HttpHeaders.IF_NONE_MATCH, etag)
-            .execute().returnResponse();
-        assertEquals(statusCode, response.getStatusLine().getStatusCode());        
-    }
-
-    private void assertUserStatus(String url, String etag, int statusCode) throws IOException { 
-        HttpResponse response = Request.Get(hostUrl + url)
-            .setHeader("Bridge-Session", user.getSession().getSessionToken())
+            .setHeader("Bridge-Session", caller.getSession().getSessionToken())
             .setHeader(HttpHeaders.IF_NONE_MATCH, etag)
             .execute().returnResponse();
         assertEquals(statusCode, response.getStatusLine().getStatusCode());        
