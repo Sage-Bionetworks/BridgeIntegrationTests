dee4620c528d1f0932c940eb9e2d6d576d5fd6d0
Dwayne Jeng <dwayne.jeng@sagebase.org>
2022-06-19 22:51:29 -0700

    BRIDGE-3260 Notifications for Study Creation in Exporter 3.0

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/Exporter3Test.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/Exporter3Test.java
@@ -8,12 +8,26 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.sagebionetworks.bridge.sdk.integration.Tests.PASSWORD;
 import static org.sagebionetworks.bridge.sdk.integration.Tests.STUDY_ID_1;
+import static org.sagebionetworks.bridge.util.IntegTestUtils.CONFIG;
 import static org.sagebionetworks.bridge.util.IntegTestUtils.TEST_APP_ID;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.PurgeQueueRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.junit.After;
@@ -31,6 +45,7 @@ import org.sagebionetworks.repo.model.table.TableEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.sagebionetworks.bridge.json.DefaultObjectMapper;
 import org.sagebionetworks.bridge.rest.ApiClientProvider;
 import org.sagebionetworks.bridge.rest.api.AccountsApi;
 import org.sagebionetworks.bridge.rest.api.AuthenticationApi;
@@ -40,18 +55,22 @@ import org.sagebionetworks.bridge.rest.api.ForConsentedUsersApi;
 import org.sagebionetworks.bridge.rest.api.ForWorkersApi;
 import org.sagebionetworks.bridge.rest.api.InternalApi;
 import org.sagebionetworks.bridge.rest.api.ParticipantsApi;
+import org.sagebionetworks.bridge.rest.api.StudiesApi;
 import org.sagebionetworks.bridge.rest.api.StudyParticipantsApi;
 import org.sagebionetworks.bridge.rest.exceptions.ConsentRequiredException;
 import org.sagebionetworks.bridge.rest.model.Account;
 import org.sagebionetworks.bridge.rest.model.App;
 import org.sagebionetworks.bridge.rest.model.ConsentSignature;
 import org.sagebionetworks.bridge.rest.model.Exporter3Configuration;
+import org.sagebionetworks.bridge.rest.model.ExporterSubscriptionRequest;
+import org.sagebionetworks.bridge.rest.model.ExporterSubscriptionResult;
 import org.sagebionetworks.bridge.rest.model.ParticipantVersion;
 import org.sagebionetworks.bridge.rest.model.Role;
 import org.sagebionetworks.bridge.rest.model.SharingScope;
 import org.sagebionetworks.bridge.rest.model.SharingScopeForm;
 import org.sagebionetworks.bridge.rest.model.SignIn;
 import org.sagebionetworks.bridge.rest.model.SignUp;
+import org.sagebionetworks.bridge.rest.model.Study;
 import org.sagebionetworks.bridge.rest.model.StudyParticipant;
 import org.sagebionetworks.bridge.rest.model.Withdrawal;
 import org.sagebionetworks.bridge.user.TestUser;
@@ -64,9 +83,14 @@ public class Exporter3Test {
     private static ForAdminsApi adminsApi;
     private static DateTime oneHourAgo;
     private static SynapseClient synapseClient;
+    private static AmazonSNS snsClient;
+    private static AmazonSQS sqsClient;
+    private static String testQueueArn;
+    private static String testQueueUrl;
     private static ForWorkersApi workersApi;
 
     private String extId;
+    private String subscriptionArn;
     private String userId;
 
     @BeforeClass
@@ -75,10 +99,27 @@ public class Exporter3Test {
         admin = TestUserHelper.getSignedInAdmin();
         adminsApi = admin.getClient(ForAdminsApi.class);
         oneHourAgo = DateTime.now().minusHours(1);
+        testQueueArn = CONFIG.get("integ.test.queue.arn");
+        testQueueUrl = CONFIG.get("integ.test.queue.url");
         workersApi = admin.getClient(ForWorkersApi.class);
+
+        // Set up AWS clients.
+        AWSCredentials awsCredentials = new BasicAWSCredentials(CONFIG.get("aws.key"),
+                CONFIG.get("aws.secret.key"));
+        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+
+        snsClient = AmazonSNSClientBuilder.standard().withCredentials(awsCredentialsProvider).build();
+        sqsClient = AmazonSQSClientBuilder.standard().withCredentials(awsCredentialsProvider).build();
 
         // Clean up stray Synapse resources before test.
         deleteEx3Resources();
+
+        // Clear queue.
+        PurgeQueueRequest purgeQueueRequest = new PurgeQueueRequest(testQueueUrl);
+        sqsClient.purgeQueue(purgeQueueRequest);
+
+        // Wait one second to ensure the queue is cleared.
+        Thread.sleep(1000);
 
         // Init Exporter 3.
         adminsApi.initExporter3().execute().body();
@@ -91,6 +132,10 @@ public class Exporter3Test {
 
     @After
     public void after() throws Exception {
+        if (subscriptionArn != null) {
+            snsClient.unsubscribe(subscriptionArn);
+        }
+
         if (userId != null) {
             admin.getClient(InternalApi.class).deleteAllParticipantVersionsForUser(userId).execute();
             adminsApi.deleteUser(userId).execute();
@@ -104,8 +149,27 @@ public class Exporter3Test {
     }
 
     private static void deleteEx3Resources() throws IOException {
+        // Delete for app.
         App app = adminsApi.getUsersApp().execute().body();
         Exporter3Configuration ex3Config = app.getExporter3Configuration();
+        deleteEx3Resources(ex3Config);
+
+        app.setExporter3Configuration(null);
+        app.setExporter3Enabled(false);
+        adminsApi.updateUsersApp(app).execute();
+
+        // Delete for study.
+        StudiesApi studiesApi = admin.getClient(StudiesApi.class);
+        Study study = studiesApi.getStudy(Tests.STUDY_ID_1).execute().body();
+        Exporter3Configuration ex3ConfigForStudy = study.getExporter3Configuration();
+        deleteEx3Resources(ex3ConfigForStudy);
+
+        study.setExporter3Configuration(null);
+        study.setExporter3Enabled(false);
+        studiesApi.updateStudy(Tests.STUDY_ID_1, study).execute();
+    }
+
+    private static void deleteEx3Resources(Exporter3Configuration ex3Config) {
         if (ex3Config == null) {
             // Exporter 3 is not configured on this app. We can skip this step.
             return;
@@ -133,20 +197,66 @@ public class Exporter3Test {
 
         // Storage locations are idempotent, so no need to delete that.
 
-        // Reset the Exporter 3 Config.
-        app.setExporter3Configuration(null);
-        app.setExporter3Enabled(false);
-        adminsApi.updateUsersApp(app).execute();
+        // Delete the Create Study SNS topic.
+        String createStudyTopicArn = ex3Config.getCreateStudyNotificationTopicArn();
+        if (createStudyTopicArn != null) {
+            try {
+                snsClient.deleteTopic(createStudyTopicArn);
+            } catch (AmazonClientException ex) {
+                LOG.error("Error deleting topic " + createStudyTopicArn);
+            }
+        }
     }
 
     @Test
     public void verifyInitExporter3() throws Exception {
-        // Verify that app has also been updated.
         App updatedApp = adminsApi.getUsersApp().execute().body();
         assertTrue(updatedApp.isExporter3Enabled());
-
-        // Verify Synapse.
         Exporter3Configuration ex3Config = updatedApp.getExporter3Configuration();
+        verifySynapseResources(ex3Config);
+    }
+
+    @Test
+    public void initForStudy() throws Exception {
+        // Subscribe to study creation.
+        ExporterSubscriptionRequest exporterSubscriptionRequest = new ExporterSubscriptionRequest();
+        exporterSubscriptionRequest.putAttributesItem("RawMessageDelivery", "true");
+        exporterSubscriptionRequest.setEndpoint(testQueueArn);
+        exporterSubscriptionRequest.setProtocol("sqs");
+        ExporterSubscriptionResult exporterSubscriptionResult = adminsApi.subscribeToCreateStudyNotifications(
+                exporterSubscriptionRequest).execute().body();
+        subscriptionArn = exporterSubscriptionResult.getSubscriptionArn();
+
+        // Init Exporter 3 for study.
+        adminsApi.initExporter3ForStudy(STUDY_ID_1).execute();
+
+        // Verify that study has been updated.
+        Study updatedStudy = adminsApi.getStudy(STUDY_ID_1).execute().body();
+        assertTrue(updatedStudy.isExporter3Enabled());
+        Exporter3Configuration ex3Config = updatedStudy.getExporter3Configuration();
+        verifySynapseResources(ex3Config);
+
+        // Verify notification in queue.
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest();
+        receiveMessageRequest.setQueueUrl(testQueueUrl);
+        receiveMessageRequest.setWaitTimeSeconds(10);
+
+        List<Message> resultList = sqsClient.receiveMessage(receiveMessageRequest).getMessages();
+        assertTrue(resultList.size() > 0);
+        Message notification = resultList.get(0);
+        String notificationJsonText = notification.getBody();
+        JsonNode notificationNode = DefaultObjectMapper.INSTANCE.readTree(notificationJsonText);
+        assertEquals(notificationNode.get("appId").textValue(), TEST_APP_ID);
+        assertEquals(notificationNode.get("parentProjectId").textValue(), ex3Config.getProjectId());
+        assertEquals(notificationNode.get("rawFolderId").textValue(), ex3Config.getRawDataFolderId());
+        assertEquals(notificationNode.get("studyId").textValue(), STUDY_ID_1);
+
+        // Delete message.
+        sqsClient.deleteMessage(testQueueUrl, notification.getReceiptHandle());
+    }
+
+    private static void verifySynapseResources(Exporter3Configuration ex3Config) throws Exception {
+        // Verify Synapse.
         long dataAccessTeamId = ex3Config.getDataAccessTeamId();
         Team dataAccessTeam = synapseClient.getTeam(String.valueOf(dataAccessTeamId));
         assertNotNull(dataAccessTeam);
