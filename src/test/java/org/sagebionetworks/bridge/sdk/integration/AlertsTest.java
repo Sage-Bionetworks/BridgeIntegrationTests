2cb6624803881b47ceccf6ca5cbc051c8edccc6e
imranmaj <imran.majeed@sagebase.org>
2023-02-03 16:03:08 -0800

    Add tests for read/unread and categories+counts

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/AlertsTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/AlertsTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.bridge.sdk.integration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.sagebionetworks.bridge.sdk.integration.Tests.STUDY_ID_1;
 import static org.sagebionetworks.bridge.util.IntegTestUtils.TEST_APP_ID;
@@ -8,6 +9,7 @@ import static org.sagebionetworks.bridge.util.IntegTestUtils.TEST_APP_ID;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.joda.time.DateTime;
@@ -24,6 +26,8 @@ import org.sagebionetworks.bridge.rest.model.ActivityEventUpdateType;
 import org.sagebionetworks.bridge.rest.model.AdherenceRecord;
 import org.sagebionetworks.bridge.rest.model.AdherenceRecordUpdates;
 import org.sagebionetworks.bridge.rest.model.Alert;
+import org.sagebionetworks.bridge.rest.model.AlertCategoriesAndCounts;
+import org.sagebionetworks.bridge.rest.model.AlertCategoryAndCount;
 import org.sagebionetworks.bridge.rest.model.Alert.CategoryEnum;
 import org.sagebionetworks.bridge.rest.model.AlertFilter;
 import org.sagebionetworks.bridge.rest.model.AlertFilter.AlertCategoriesEnum;
@@ -47,6 +51,7 @@ import org.sagebionetworks.bridge.user.TestUser;
 import org.sagebionetworks.bridge.user.TestUserHelper;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class AlertsTest {
     private static final String CUSTOM_EVENT = "custom:event1";
@@ -56,6 +61,8 @@ public class AlertsTest {
     private TestUser worker;
     private TestUser user;
     private TestUser user2;
+    private TestUser user3;
+    private TestUser user4;
     private TestUser developer;
 
     private AlertsApi researcherAlertsApi;
@@ -144,6 +151,12 @@ public class AlertsTest {
         user.signOutAndDeleteUser();
         if (user2 != null) {
             user2.signOutAndDeleteUser();
+        }
+        if (user3 != null) {
+            user3.signOutAndDeleteUser();
+        }
+        if (user4 != null) {
+            user4.signOutAndDeleteUser();
         }
         developer.signOutAndDeleteUser();
     }
@@ -376,6 +389,151 @@ public class AlertsTest {
         assertEquals(2, alerts.getItems().size());
         assertOneMatchingAlert(alerts, Alert.CategoryEnum.NEW_ENROLLMENT, user2.getUserId());
         assertOneMatchingAlert(alerts, Alert.CategoryEnum.TIMELINE_ACCESSED, user.getUserId());
+    }
+
+    @Test
+    public void readUnreadAndCategoryCounts() throws IOException {
+        // clear existing alerts
+        deleteAlerts();
+
+        // new enrollment alert
+        user3 = TestUserHelper.createAndSignInUser(AlertsTest.class, true);
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 1));
+
+        // new enrollment alert
+        user4 = TestUserHelper.createAndSignInUser(AlertsTest.class, true);
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2));
+
+        // trigger low adherence alert
+        workerApi.getWeeklyAdherenceReportForWorker(admin.getAppId(), STUDY_ID_1, user3.getUserId()).execute();
+
+        // verify 1 low adherence alert
+        AlertList alerts = researcherAlertsApi
+                .getAlerts(STUDY_ID_1, new AlertFilter().alertCategories(ImmutableList.of()), 0, 100).execute().body();
+        Alert lowAdherenceAlert1 = assertOneMatchingAlert(alerts, CategoryEnum.LOW_ADHERENCE, user3.getUserId());
+        // alert should be unread
+        assertFalse(lowAdherenceAlert1.isRead());
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2,
+                AlertCategoryAndCount.CategoryEnum.LOW_ADHERENCE, 1));
+
+        // trigger low adherence alert again, should overwrite alert (testing
+        // overwriting in unread state)
+        workerApi.getWeeklyAdherenceReportForWorker(admin.getAppId(), STUDY_ID_1, user3.getUserId()).execute();
+
+        // verify new adherence alert
+        alerts = researcherAlertsApi
+                .getAlerts(STUDY_ID_1, new AlertFilter().alertCategories(ImmutableList.of()), 0, 100).execute().body();
+        Alert lowAdherenceAlert2 = assertOneMatchingAlert(alerts, CategoryEnum.LOW_ADHERENCE, user3.getUserId());
+        // alert should be unread
+        assertFalse(lowAdherenceAlert2.isRead());
+        // alert should be newer
+        assertTrue(lowAdherenceAlert2.getCreatedOn().isAfter(lowAdherenceAlert1.getCreatedOn()));
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2,
+                AlertCategoryAndCount.CategoryEnum.LOW_ADHERENCE, 1));
+
+        // mark as read
+        researcherAlertsApi.markAlertsRead(STUDY_ID_1,
+                new AlertIdCollection().alertIds(ImmutableList.of(lowAdherenceAlert2.getId()))).execute();
+        // make sure alert is marked as read but otherwise unchanged
+        alerts = researcherAlertsApi
+                .getAlerts(STUDY_ID_1, new AlertFilter().alertCategories(ImmutableList.of()), 0, 100).execute().body();
+        Alert lowAdherenceAlert2Read = assertOneMatchingAlert(alerts, CategoryEnum.LOW_ADHERENCE, user3.getUserId());
+        assertTrue(lowAdherenceAlert2Read.isRead());
+        assertEquals(lowAdherenceAlert2.getId(), lowAdherenceAlert2Read.getId());
+        assertEquals(lowAdherenceAlert2.getCategory(), lowAdherenceAlert2Read.getCategory());
+        assertEquals(lowAdherenceAlert2.getParticipant().getIdentifier(),
+                lowAdherenceAlert2Read.getParticipant().getIdentifier());
+        assertEquals(lowAdherenceAlert2.getCreatedOn(), lowAdherenceAlert2Read.getCreatedOn());
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2,
+                AlertCategoryAndCount.CategoryEnum.LOW_ADHERENCE, 1));
+
+        // trigger low adherence alert again, should overwrite alert (testing
+        // overwriting in read state)
+        workerApi.getWeeklyAdherenceReportForWorker(admin.getAppId(), STUDY_ID_1, user3.getUserId()).execute();
+
+        // verify new adherence alert
+        alerts = researcherAlertsApi
+                .getAlerts(STUDY_ID_1, new AlertFilter().alertCategories(ImmutableList.of()), 0, 100).execute().body();
+        Alert lowAdherenceAlert3 = assertOneMatchingAlert(alerts, CategoryEnum.LOW_ADHERENCE, user3.getUserId());
+        // alert should be unread
+        assertFalse(lowAdherenceAlert3.isRead());
+        // alert should be newer
+        assertTrue(lowAdherenceAlert3.getCreatedOn().isAfter(lowAdherenceAlert2.getCreatedOn()));
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2,
+                AlertCategoryAndCount.CategoryEnum.LOW_ADHERENCE, 1));
+
+        // still need to test unread -> read -> unread
+        // mark as read
+        researcherAlertsApi.markAlertsRead(STUDY_ID_1,
+                new AlertIdCollection().alertIds(ImmutableList.of(lowAdherenceAlert3.getId()))).execute();
+        // make sure alert is marked as read but otherwise unchanged
+        alerts = researcherAlertsApi
+                .getAlerts(STUDY_ID_1, new AlertFilter().alertCategories(ImmutableList.of()), 0, 100).execute().body();
+        Alert lowAdherenceAlert3Read = assertOneMatchingAlert(alerts, CategoryEnum.LOW_ADHERENCE, user3.getUserId());
+        assertTrue(lowAdherenceAlert3Read.isRead());
+        assertEquals(lowAdherenceAlert3.getId(), lowAdherenceAlert3Read.getId());
+        assertEquals(lowAdherenceAlert3.getCategory(), lowAdherenceAlert3Read.getCategory());
+        assertEquals(lowAdherenceAlert3.getParticipant().getIdentifier(),
+                lowAdherenceAlert3Read.getParticipant().getIdentifier());
+        assertEquals(lowAdherenceAlert3.getCreatedOn(), lowAdherenceAlert3Read.getCreatedOn());
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2,
+                AlertCategoryAndCount.CategoryEnum.LOW_ADHERENCE, 1));
+
+        // mark as unread
+        researcherAlertsApi.markAlertsUnread(STUDY_ID_1,
+                new AlertIdCollection().alertIds(ImmutableList.of(lowAdherenceAlert3.getId()))).execute();
+        // make sure alert is marked as unread but otherwise unchanged
+        alerts = researcherAlertsApi
+                .getAlerts(STUDY_ID_1, new AlertFilter().alertCategories(ImmutableList.of()), 0, 100).execute().body();
+        Alert lowAdherenceAlert3Unread = assertOneMatchingAlert(alerts, CategoryEnum.LOW_ADHERENCE, user3.getUserId());
+        assertFalse(lowAdherenceAlert3Unread.isRead());
+        assertEquals(lowAdherenceAlert3.getId(), lowAdherenceAlert3Unread.getId());
+        assertEquals(lowAdherenceAlert3.getCategory(), lowAdherenceAlert3Unread.getCategory());
+        assertEquals(lowAdherenceAlert3.getParticipant().getIdentifier(),
+                lowAdherenceAlert3Unread.getParticipant().getIdentifier());
+        assertEquals(lowAdherenceAlert3.getCreatedOn(), lowAdherenceAlert3Unread.getCreatedOn());
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2,
+                AlertCategoryAndCount.CategoryEnum.LOW_ADHERENCE, 1));
+
+        // delete low adherence alert
+        researcherAlertsApi.deleteAlerts(STUDY_ID_1,
+                new AlertIdCollection().alertIds(ImmutableList.of(lowAdherenceAlert3.getId()))).execute();
+
+        // verify deletion
+        alerts = researcherAlertsApi
+                .getAlerts(STUDY_ID_1, new AlertFilter().alertCategories(ImmutableList.of()), 0, 100).execute().body();
+        assertNoMatchingAlerts(alerts, CategoryEnum.LOW_ADHERENCE, user3.getUserId());
+
+        // check categories and counts
+        assertCategoriesAndCounts(ImmutableMap.of(AlertCategoryAndCount.CategoryEnum.NEW_ENROLLMENT, 2));
+    }
+
+    private void assertCategoriesAndCounts(Map<AlertCategoryAndCount.CategoryEnum, Integer> expectedCategoriesAndCounts)
+            throws IOException {
+        AlertCategoriesAndCounts alertCategoriesAndCounts = researcherAlertsApi.getAlertCategoriesAndCounts(STUDY_ID_1)
+                .execute().body();
+        assertEquals(expectedCategoriesAndCounts.size(), alertCategoriesAndCounts.getAlertCategoriesAndCounts().size());
+        for (AlertCategoryAndCount categoryAndCount : alertCategoriesAndCounts.getAlertCategoriesAndCounts()) {
+            assertTrue(expectedCategoriesAndCounts.containsKey(categoryAndCount.getCategory()));
+            assertTrue(expectedCategoriesAndCounts.get(categoryAndCount.getCategory())
+                    .equals(categoryAndCount.getCount()));
+        }
     }
 
     private Alert assertOneMatchingAlert(AlertList alerts, CategoryEnum category, String userId) {
